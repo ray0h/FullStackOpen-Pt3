bbd13924f3db0e7e23779bb436d35af28d3f130d
Raymond Oh <raymond.oh@gmail.com>
2020-04-15 14:17:52 -0400

    update with build folder included

---
+++ build/precache-manifest.c966a9f81a5953d3979c050910180b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4af898e51f78b58fc5f631eb004b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26eccee02ad11f960fe8",
+    "url": "/static/css/main.2800187d.chunk.css"
+  },
+  {
+    "revision": "51f7f301c540920ff627",
+    "url": "/static/js/2.2165f8ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2165f8ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26eccee02ad11f960fe8",
+    "url": "/static/js/main.b814d7e6.chunk.js"
+  },
+  {
+    "revision": "118538f51c86fd2ac789",
+    "url": "/static/js/runtime-main.c796d84a.js"
+  }
+]);
